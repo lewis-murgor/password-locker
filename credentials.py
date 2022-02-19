c7ee7c50c2d17d3ca60a650bb615765bed639b5e
Lewis <lewis.murgor@student.moringaschool.com>
2022-02-20 01:46:10 +0300

    create credentials class

---
+++ credentials.py
@@ -0,0 +1,11 @@
+
+
+class Credentials:
+
+    Credentials_list = []
+
+    def __init__(self, user_password, credential_name, credential_password):
+
+        self.user_password = user_password
+        self.credential_name = credential_name
+        self.credential_password = credential_password
