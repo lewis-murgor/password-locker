293a6205899737350fc0650d2c00bff72c89f4fd
Lewis <lewis.murgor@student.moringaschool.com>
2022-02-20 20:22:21 +0300

    create setUp method

--- credentials_test.py
+++ credentials_test.py
@@ -5,3 +5,18 @@ from credentials import Credentials
 import unittest to create tests for credentials
 import Credentials class to be tested
 '''
+
+class TestCredentials(unittest.TestCase):
+    '''
+    Test class that defines test cases for the credentials class behaviours.
+
+    Args:
+        unittest.TestCase: TestCase class that helps in creating test cases
+    '''
+
+    def setUp(self):
+        '''
+        Set up method to run before each test case
+        '''
+
+        self.new_credential = Credentials("murgor", "instagram", "POg6oom!")
