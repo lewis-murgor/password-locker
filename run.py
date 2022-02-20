702d898e7118750015dc33abd95b5356661afdb1
Lewis <lewis.murgor@student.moringaschool.com>
2022-02-21 01:06:19 +0300

    create user functions

--- run.py
+++ run.py
@@ -19,3 +19,43 @@ def create_user(name, password):
     new_user = User(name, password)
 
     return new_user
+
+def save_users(user):
+    '''
+    Function that saves a user account
+
+    Args:
+        user: user account to be saved
+    '''
+
+    User.save_user()
+
+def check_existing_users(name):
+    '''
+    Function that checks if a username already exists
+
+    Args:
+        name: user account name
+    '''
+
+    return User.user_exists(name)
+
+def user_log_in(name, password):
+    '''
+    Function that allows users to login to their account
+
+    Args:
+        name: name used to create user account
+        password: password used to create user account
+    '''
+
+    log_in = User.log_in(name, password)
+    if log_in != False:
+        return User.log_in(name, password)
+
+def display_users():
+    '''
+    Function that returns all saved users
+    '''
+
+    return User.display_user()
